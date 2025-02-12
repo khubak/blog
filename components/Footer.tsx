2f29ee6b4c08137c8b3d06087d8a693f9ae2eb68
Karlo Hubak <103487011+khubak@users.noreply.github.com>
2025-02-12 16:06:03 +0100

    fix: footer margins

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className='container mx-auto px-4 py-8 border-t border-gray-800 mt-20'>
+    <footer className='container mx-auto px-4 py-8 border-t border-gray-800 mt-6 md:mt-10'>
       <div className='flex flex-col items-center justify-center gap-4'>
         <p className='text-sm text-gray-400'>© {new Date().getFullYear()} Miracle Tree. All rights reserved.</p>
       </div>
